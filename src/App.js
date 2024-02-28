c6520cff48fd8789082d783505f460e0dd321da6
MELATONIN99 <sindogfood@naver.com>
2024-02-29 01:34:03 +0900

    fix. 새로고침 시 데이터를 먼저 받아 올 수 있도록 수정, 로딩 페이지 임시 구현

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 import Router from "./component/Router.js";
+import { auth } from "./firebase/firebase.js";
+import styled from "styled-components";
+const LoadingScreen = styled.div`
 
+background-color: white;
+color: blue;
+`
 function App() {
+  const [isLoding, setLoading] = useState(true);
+  const init = async() => {
+    await auth.authStateReady();
+    setLoading(false);
+  };
+  useEffect(() => {
+    init();
+  },[]);
   return (
- <Router></Router>
+    <div>
+ {isLoding ? <LoadingScreen/> :<Router/>}
+ </div>
   );
 }
 
