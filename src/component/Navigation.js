9fffe5a368233cd70f6f812d05c1e023acd49c58
MELATONIN99 <sindogfood@naver.com>
2024-03-30 02:44:43 +0900

    fix. 로그아웃 시 비로그인 상태로 메인 페이지로 이동되는 버그 수정

--- src/component/Navigation.js
+++ src/component/Navigation.js
@@ -1,12 +1,21 @@
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import { MainLogo, Menu, Nav } from './ComponentStyle'  
+import { auth } from '../firebase/firebase';
+import { useEffect } from 'react';
 
 
 const logoimg = "MELATONINLOGO.png";
 
 
 export default function Navigation() {
-    return(
+  const navigate = useNavigate();
+  const user = auth.currentUser
+  useEffect(() => {
+    if(user === null){
+        navigate("/login")
+    }
+  },{})
+return(
 
 <Nav>
   <MainLogo>
@@ -17,15 +26,19 @@ export default function Navigation() {
 
       <Menu>
         <Link className="nav-link" to="/">Home</Link>
-        </Menu>
+      </Menu>
       
       <Menu>
         <Link className="nav-link" to="/momentum">Momentum</Link>
-        </Menu>
-  
+      </Menu>
+      
+      <Menu>  
+        <Link className="nav-link" to="/diary">Diary</Link>
+      </Menu>
+
       <Menu>
         <Link className="nav-link" to="/profile">Profile</Link>
-        </Menu>
+      </Menu>
 </Nav>
     )
 }
