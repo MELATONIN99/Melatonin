00d94c1b0187c99fc5350cc28349f0312661d1a3
MELATONIN99 <sindogfood@naver.com>
2024-03-30 02:31:43 +0900

    build. MUI라이브러리 설치, 적용

--- src/App.js
+++ src/App.js
@@ -3,24 +3,30 @@ import "./App.css";
 import Router from "./component/Router.js";
 import { auth } from "./firebase/firebase.js";
 import styled from "styled-components";
+import { LocalizationProvider } from "@mui/x-date-pickers";
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
+import {ko} from 'date-fns/locale/ko';
 const LoadingScreen = styled.div`
 
 background-color: white;
 color: blue;
 `
 function App() {
-  const [isLoding, setLoading] = useState(true);
+  const [isLoding, setisLoading] = useState(true);
   const init = async() => {
     await auth.authStateReady();
-    setLoading(false);
+    setisLoading(false);
   };
   useEffect(() => {
     init();
+    const user = auth.currentUser
   },[]);
   return (
+    <LocalizationProvider dateAdapter={AdapterDayjs} adapterLocale={ko}>
     <div>
  {isLoding ? <LoadingScreen/> :<Router/>}
  </div>
+ </LocalizationProvider>
   );
 }
 
